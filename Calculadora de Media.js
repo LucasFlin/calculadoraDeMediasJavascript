ab886afdd56704e4273405b14557c9f20d6adb00
Lucas Flin <malucasnba@gmail.com>
2025-03-26 22:50:13 -0300

    Adicionada a validação se a média é maior que 6 e mostra de o usuário foi aprovado ou reprovado.

--- Calculadora de Media.js
+++ Calculadora de Media.js
@@ -3,4 +3,8 @@ let nota1 = prompt("Digite a primeira nota: ")*1
 let nota2 = prompt("Digite a segunda nota: ")*1
 let media = (nota1 + nota2)/2
 
-console.log(`media: ${media}`)
+if(media>=6){
+    console.log(`Sua média é ${media}, você foi aprovado(a)!`)
+}else{
+    console.log(`Sua média é ${media}, você foi reprovado(a)...`)
+}

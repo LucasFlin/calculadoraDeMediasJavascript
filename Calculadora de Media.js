3a530826964df30e5683e86767623017b0e29dbb
Lucas Flin <malucasnba@gmail.com>
2025-03-26 22:38:09 -0300

    Adicionei as variaveis que receberão as notas, e uma que calcula e registra a média

--- Calculadora de Media.js
+++ Calculadora de Media.js
@@ -1,2 +1,6 @@
 const {prompt} = require('vscode-websocket-alerts')
-prompt("Teste")
+let nota1 = prompt("Digite a primeira nota: ")*1
+let nota2 = prompt("Digite a segunda nota: ")*1
+let media = (nota1 + nota2)/2
+
+console.log(`media: ${media}`)
